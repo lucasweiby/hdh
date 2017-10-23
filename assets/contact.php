c211c5f17981d3b64c6ba2a47db80fcb8fe83561
PET Computação UFC <petcomputacaoufc@gmail.com>
2017-10-23 18:26:55 -0300

    Contact form changes

--- assets/contact.php
+++ assets/contact.php
@@ -1,35 +1,39 @@
 <section id="contact">
 	<div class="container">
 		<div class="row">
-			<div class="text-contact">
-				<h1 class="um">Alguma dúvida?</h1>
-				<p class="dois">Entre em contato conosco!</p>
+			<div class="col lg6 m6">
+				<form class="margem col lg12 m12 s12">
+					<div class="row">
+						<div class="input-field col lg6 m6 s6">
+							<input id="first_name" type="text" class="validate">
+							<label for="first_name">Nome</label>
+						</div>
+						<div class="input-field col lg6 m6 s6">
+							<input id="last_name" type="text" class="validate">
+							<label for="last_name">Sobrenome</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="input-field col s12">
+							<input id="email" type="email" class="validate">
+							<label for="email">E-mail</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="input-field col s12">
+							<textarea id="textarea1" class="materialize-textarea"></textarea>
+							<label for="textarea1">Mensagem</label>
+						</div>
+					</div>
+					<input type="submit" class="hvr-grow waves-effect waves-light btn" value="Enviar" />    
+				</form>
 			</div>
-			<form class="margem col lg12 m12 s12">
-				<div class="row">
-					<div class="input-field col lg6 m6 s6">
-						<input id="first_name" type="text" class="validate">
-						<label for="first_name">Nome</label>
-					</div>
-					<div class="input-field col lg6 m6 s6">
-						<input id="last_name" type="text" class="validate">
-						<label for="last_name">Sobrenome</label>
-					</div>
+			<div class="col lg6 m6 right-align">
+				<div class="text-contact">
+					<h1 class="um">Alguma dúvida?</h1>
+					<p class="dois">Entre em contato conosco!</p>
 				</div>
-				<div class="row">
-					<div class="input-field col s12">
-						<input id="email" type="email" class="validate">
-						<label for="email">E-mail</label>
-					</div>
-				</div>
-				<div class="row">
-					<div class="input-field col s12">
-						<textarea id="textarea1" class="materialize-textarea"></textarea>
-						<label for="textarea1">Mensagem</label>
-					</div>
-				</div>
-				<input type="submit" class="hvr-grow waves-effect waves-light btn" value="Enviar" />    
-			</form>
+			</div>
 		</div>
 	</div>
 </section>
