7fed62094da5f69cb5aa2d1701a064d745434160
Débora Moura <deboramoura@alu.ufc.br>
2017-10-24 00:02:02 -0300

    Contato

--- send_email.php
+++ send_email.php
@@ -4,7 +4,7 @@
 	$subject = "Contato Hana dul Hallyu";
 	$first_name = $_POST['first_name'];
 	$last_name = $_POST['last_name'];
-	$sender = $_POST['sender'];
+	$sender = $_POST['email'];
 	$message = $_POST['message'];
 
 
@@ -13,8 +13,11 @@
 	$body .="<h6><b>Email: </b></h6><p>$sender</p>";
 	$body .="<h6><b>Mensagem: </b></h6><p>$message</p>";
 
-	$header = "From: $sender Reply-to: $sender";
+	$header = "From:$sender Reply-to:$sender";
 	$header .= "Content-Type: text/html; charset= utf-8";
 
 	mail($to,$subject,$body,$header);
+
+	header('Location: index.php#contact');
+
 ?>
