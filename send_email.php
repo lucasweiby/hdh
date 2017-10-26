0ffcd49e04c2d271a2080b24ce2c6e68e063f46d
Débora Moura <deboramoura@alu.ufc.br>
2017-10-25 21:24:11 -0300

    Contato email

--- send_email.php
+++ send_email.php
@@ -16,7 +16,7 @@ $hora_envio = date('H:i:s');
   margin:0px;
   font-family:Verdane;
   font-size:12px;
-  color: #666666;
+  color: #ccc;
   }
   a{
   color: #666666;
@@ -29,19 +29,15 @@ $hora_envio = date('H:i:s');
   </style>
     <html>
         <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
-            <tr>
-              <td>
-  				<tr>
-                 <td width='500'>Nome: $first_name $last_name</td>
-                </tr>
-                <tr>
-                  <td width='320'>E-mail: <b>$email</b></td>
-     			</tr>
-                <tr>
-                  <td width='320'>Mensagem: $nome</td>
-                </tr>
-            </td>
-          </tr>  
+		      <tr>
+           <td width='500'>Nome: $first_name $last_name</td>
+          </tr>
+          <tr>
+            <td width='320'>E-mail: <b>$email</b></td>
+			      </tr>
+          <tr>
+            <td width='320'>Mensagem: $message</td>
+          </tr> 
           <tr>
             <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
           </tr>
@@ -59,7 +55,7 @@ $hora_envio = date('H:i:s');
   // É necessário indicar que o formato do e-mail é html
   $headers  = 'MIME-Version: 1.0' . "\r\n";
       $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-      $headers .= 'From: $nome <$email>';
+      $headers .= 'From: $first_name $last_name < $email >';
   
   //$headers .= "Bcc: $EmailPadrao\r\n";
   
