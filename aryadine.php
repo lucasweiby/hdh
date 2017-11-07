ff32db3d17c2856153c71d438836725524f4aaa0
lucasweiby <lucasweiby@ajatdesenvolvimento.com.br>
2017-11-06 23:13:24 -0300

    Changes

--- aryadine.php
+++ aryadine.php
@@ -3,8 +3,8 @@
 <head>
 	<?php include 'assets/head.php'; ?>
 </head>
-<body>
-	<div class="container">
+<body class="workshops">
+	<div class="container ary">
 		<div class="row">
 			<div class="picture col xl12 l12 m12 s12">
 				<img class="team-pic circle responsive-img" src="img/workshops/aryadine.jpg">
@@ -13,16 +13,19 @@
 		</div>
 
 		<div class="row">
-			<div class="desc col xl6 l6 m6 s6">
+			<div class="desc col xl6 l6 m12 s12">
 				<p class="text">
 					<strong style="font-weight: bold;">Maria Aryadine Venancio Bezerra</strong> inicou os estudos de percussão sinfônica aos 8 anos de idade (aproximadamente em 2004) no Grupo Infanto-Juventil de Percussão: Marimbas, em Maracanaú. Aos 14 anos participou do seu primeiro festival: Festival Eleazar de Carvalho, em Fortaleza e no mesmo ano foi convidada pelo maestro Alfredo Barros para tocar na então Orquestra Contemporânea do Ceará (atual Orquestra Sinfônica da Universidade Estadual do Ceará - OSUECE). Participou de duas edições do festival, tocando na orquestra do festival sob a regência de maestros internacionais como Zimmermman e brasileiros como Rafael Luz e Sergei Eleazar de Carvalho. Teve como professor o músico solista francês, Thierry Miroglio.
 				</p>
 				<p class="text">
 					Após sua entrada na OSUECE, tocou com músicos conhecidos local, nacional e internacionalmente, tais como Marina de la Riva, Chambinha (interpretou Luiz Gonzaga no filme: Gonzaga, de Pai pra Filho), Waldonys, Adelsson Viana e outros solistas da Universidade Estadual do Ceará. Tocou como musicista convidada na Orquestra Eleazar de Carvalho diversas vezes, onde participou de concertos com solistas convidados (Emmanuelle Baldini: spalla da Orquestra Sinfônica de São Paulo – OSESP), estreia de composição feita para Contrabaixo e Orquestra de Arthur Barbosa, concertos de Natal. 
 				</p>
+				<p class="text">
+					Recentemente participou de concerto com grupo Ensamble de Solistas de la Escuela Superior de Música José Lo Giúdice, do concerto em homenagem à Humberto Teixeira no Dragão do Mar e da última turnê o tenor José Carreras. Atua como chefe de naipe da percussão da OSUECE e vocalista na Banda ADORERS.
+				</p>
 			</div>
-			<div class="img-desc col xl6 l6 m6 s6">
-				<img class="responsive-img materialboxed" src="img/workshops/a1.jpg">
+			<div class="img-desc col xl6 l6 m12 s12">
+				<img class="responsive-img" src="img/workshops/a1.jpg">
 			</div>
 		</div>
 	</div>
