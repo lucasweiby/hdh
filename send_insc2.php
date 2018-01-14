1970c8d3d6fd8ceca8e4452d278b0f0afa11ef72
Débora Moura <deboramoura@alu.ufc.br>
2018-01-13 21:06:16 -0300

    teste formulario

--- send_insc2.php
+++ send_insc2.php
@@ -1,115 +1,93 @@
-<?php
 
-//Variáveis
-$name = $_POST['name'];
-$categoria = $_POST['categoria'];
-$email = $_POST['email'];
-$integ = $_POST['integ'];
+<?php 
+ 
+//Pega os dados postados pelo formulário HTML e os coloca em variaveis
+if (preg_match('/hanadulhallyu.com.br$/', $_SERVER[HTTP_HOST])) {
+//substitua na linha acima a aprte locaweb.com.br por seu domínio.
+$email_from='hanadulhallyu@gmail.com';  // Substitua essa linha pelo seu e-mail@seudominio
+}else {
+$email_from = "contato@" . $_SERVER[HTTP_HOST];         
+//    Na linha acima estamos forçando que o remetente seja 'webmaster@',
+// você pode alterar para que o remetente seja, por exemplo, 'contato@'.
+}
+ 
+ 
+if( PATH_SEPARATOR ==';'){ $quebra_linha="\r\n";
+ 
+} elseif (PATH_SEPARATOR==':'){ $quebra_linha="\n";
+ 
+} elseif ( PATH_SEPARATOR!=';' and PATH_SEPARATOR!=':' )  {echo ('Esse script não funcionará corretamente neste servidor, a função PATH_SEPARATOR não retornou o parâmetro esperado.');
+ 
+}
+ 
+//pego os dados enviados pelo formulário 
+  $name       = $_POST['name'];
+  $categoria  = $_POST['categoria'];
+  $email_rep      = $_POST['email'];
+  $integ      = $_POST['integ'];
 
-$link = $_POST['link'];
-$coreografo = $_POST['coreografo'];
+  $link       = $_POST['link'];
+  $coreografo = $_POST['coreografo'];
 
-$data_envio = date('d/m/Y');
-$hora_envio = date('H:i:s');
-
-// Compo E-mail
-  $arquivo = "
-  <style type='text/css'>
-  body {
-  margin:0px;
-  font-family:Verdane;
-  font-size:12px;
-  color: #666;
-  }
-  a{
-  color: #666666;
-  text-decoration: none;
-  }
-  a:hover {
-  color: #FF0000;
-  text-decoration: none;
-  }
-  </style>
-    <html>
-        <table width='510' cellpadding='1' cellspacing='1'>
-		      <tr>
-           <td width='500'>Nome: $name</td>
-          </tr>
-          <tr>
-            <td width='320'>E-mail: <b>$email</b></td>
-			      </tr>
-          <tr>
-            <td width='320'>Categoria: $categoria</td>
-          </tr> 
-          <tr>
-            <td width='320'>Categoria: $categoria</td>
-          </tr>
-          <tr>
-            <td width='320'>Integrantes: $integrantes</td>
-          </tr>
-          <tr>
-            <td width='320'>Link: $link</td>
-          </tr>
-          <tr>
-            <td width='320'>Coreógrafo: $coreografo</td>
-          </tr>
-          <tr>
-            <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
-          </tr>
-        </table>
-    </html>
-  ";
-
-  //enviar
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
   
-  // emails para quem será enviado o formulário
-  //$emailenviar = "lucas13662@gmail.com";
-  $emailenviar = "deboramoura@alu.ufc.br";
-  $destino = $emailenviar;
-  $assunto = "Inscrição - Hana dul Hallyu";
+  //$email = "lucas13662@gmail.com";
+  $email = "deboramoura@alu.ufc.br";
+  $assunto = 'Inscrição - Hana dul Hallyu';
+ 
+$arquivo = isset($_FILES["anexo"]) ? $_FILES["anexo"] : FALSE; 
+ 
+if(file_exists($arquivo["tmp_name"]) and !empty($arquivo)){ 
 
-  //ARQUIVO -------------------------------------------
+$fp = fopen($_FILES["anexo"]["tmp_name"],"rb"); 
+$anexo = fread($fp,filesize($_FILES["anexo"]["tmp_name"])); 
+$anexo = base64_encode($anexo); 
+ 
+fclose($fp); 
+ 
+$anexo = chunk_split($anexo); 
+ 
+ 
+$boundary = "XYZ-" . date("dmYis") . "-ZYX"; 
+ 
+$mens = "--$boundary" . $quebra_linha . ""; 
+$mens .= "Content-Transfer-Encoding: 8bits" . $quebra_linha . ""; 
+$mens .= "Content-Type: text/html; charset=\"utf-8\"" . $quebra_linha . "" . $quebra_linha . ""; 
+$mens .= "<br>Nome do Grupo: $name" . $quebra_linha . ""; 
+$mens .= "<br>Categoria: $categoria" . $quebra_linha . ""; 
+$mens .= "<br>Email do representante: $email_rep" . $quebra_linha . ""; 
+$mens .= "<br>Nº de integrantes: $integ" . $quebra_linha . "";
+$mens .= "<br>Link da Coreografia: $link" . $quebra_linha . ""; 
+$mens .= "<br>Nome do Coreografo: $coreografo" . $quebra_linha . ""; 
+$mens .= "<br>Enviado em: $data_envio às $hora_envio" . $quebra_linha . ""; 
+$mens .= "--$boundary" . $quebra_linha . ""; 
+$mens .= "Content-Type: ".$arquivo["type"]."" . $quebra_linha . ""; 
+$mens .= "Content-Disposition: attachment; filename=\"".$arquivo["name"]."\"" . $quebra_linha . ""; 
+$mens .= "Content-Transfer-Encoding: base64" . $quebra_linha . "" . $quebra_linha . ""; 
+$mens .= "$anexo" . $quebra_linha . ""; 
+$mens .= "--$boundary--" . $quebra_linha . ""; 
+ 
+$headers = "MIME-Version: 1.0" . $quebra_linha . ""; 
+$headers .= "From: $email_from " . $quebra_linha . ""; 
+$headers .= "Return-Path: $email_from " . $quebra_linha . ""; 
+$headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"" . $quebra_linha . ""; 
+$headers .= "$boundary" . $quebra_linha . ""; 
+ 
+//envio o email com o anexo 
+mail($email,$assunto,$mens,$headers, "-r".$email_rep); 
+ 
+//echo"Email enviado com Sucesso!"; 
+header('Location: orcamento_enviado.php');
+ 
+} 
+ 
 
-  $boundary = "XYZ-".md5(date("dmYis"))."-ZYX";
-  // Arquivo enviado via formulário
-  $path = $_FILES['anexo']['tmp_name']; 
-  $fileType = $_FILES['anexo']['type']; 
-  $fileName = $_FILES['anexo']['name']; 
 
-  // Pegando o conteúdo do arquivo
-  $fp = fopen( $path, "rb" ); // abre o arquivo enviado
-  $anexo = fread( $fp, filesize( $path ) ); // calcula o tamanho
-  $anexo = chunk_split(base64_encode( $anexo )); // codifica o anexo em base 64
-  fclose( $fp ); // fecha o arquivo
-
-
-  $headers = "MIME-Version: 1.0" . PHP_EOL;
-  $headers .= "Content-Type: multipart/mixed; ";
-  $headers .= 'From:'. $first_name . '<'.$email.'>';
-  $headers .= "boundary=" . $boundary . PHP_EOL;
-  $headers .= "$boundary" . PHP_EOL;
-
-  $mensagem  = "--$boundary" . PHP_EOL;
-  $mensagem .= "Content-Type: text/html; charset='utf-8'" . PHP_EOL;
-  $mensagem .= "UM TEXTO ALEATORIO QUALQUER"; // Adicione aqui sua mensagem
-  $mensagem .= "--$boundary" . PHP_EOL;
-
-  $mensagem .= "Content-Type: ". $fileType ."; name=\"". $fileName . "\"" . PHP_EOL;
-  $mensagem .= "Content-Transfer-Encoding: base64" . PHP_EOL;
-  $mensagem .= "Content-Disposition: attachment; filename=\"". $fileName . "\"" . PHP_EOL;
-  $mensagem .= "$anexo" . PHP_EOL;
-  $mensagem .= "--$boundary" . PHP_EOL;
-
-  //ARQUIVO FIM-------------------------------------------
-  
-  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
-  if($enviaremail){
-  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-  echo "$msg";
-  //echo " <meta http-equiv='refresh' content='10;URL=index.php#contact'>";
-  } else {
-  $mgm = "ERRO AO ENVIAR E-MAIL!";
-  echo "$msg";
-  //echo "";
-  }
+//se nao tiver anexo 
+else{ 
+ 
+header('Location: sem_anexo.php');
+} 
+ 
 ?>
