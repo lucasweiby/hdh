1be02178430aef833ccae20dabae704ccd31b49a
lucasweiby <lucasweiby@ajatdesenvolvimento.com.br>
2018-01-30 00:31:47 -0300

    Workshops

--- assets/convidados.php
+++ assets/convidados.php
@@ -1,60 +1,47 @@
-<section id="convidados" class="convidados hide">
+<section id="convidados" class="convidados">
 	<div class="container">
 		<h2 class="workshops">WORKSHOPS</h2>
 		<div class="line"></div>
 		<div class="row">
-			<div class="box col xl3 l3 m6 s12">
+			<div class="box col xl4 l4 m12 s12">
 				<div class="team-member hvr-grow" >
 					<div class="image">
-						<img src="img/workshops/aryadine.jpg" class="team-pic circle responsive-img">
+						<img src="img/workshops/3ed/nina.jpg" class="team-pic circle responsive-img">
 					</div>
-					<h3 class="team-name">Aryadine Venancio</h3>
-					<p class="team-function">Técnica Vocal</p>
-					<div class="text">
+					<h3 class="team-name">Nina Andrade</h3>
+					<p class="team-function">K-Pop Girls</p>
+					<!-- <div class="text">
 						<p class="team-desc">Vocalista da Banda Adores<br>Percursionista da Orquestra Sinfônica da UECE</p>
-					</div>
+					</div> -->
 				</div>
 				<!-- /.team-member -->
 			</div>
-			<div class="box col xl3 l3 m6 s12">
+			<div class="box col xl4 l4 m12 s12">
 				<div class="team-member" hvr-grow >
 					<div class="image">
-						<img src="img/workshops/norbit.jpg" class="team-pic circle responsive-img">
+						<img src="img/workshops/2ed/norbit.jpg" class="team-pic circle responsive-img">
 					</div>
 					<h3 class="team-name">Nalbert Albuquerque</h3>
-					<p class="team-function">K-POP</p>
-					<div class="text">
+					<p class="team-function">Hip Hop Contemporâneo</p>
+					<!-- <div class="text">
 						<p class="team-desc">Professor da Mova Dance Studio<br>Dançarino do Tsubasa Dance Group<br>1º Grande Lugar no K-POP World Festival - 2016</p>
-					</div>
+					</div> -->
 				</div>
 				<!-- /.team-member -->
 			</div>
-			<div class="box col xl3 l3 m6 s12">
+			<div class="box col xl4 l4 m12 s12">
 				<div class="team-member hvr-grow" >
 					<div class="image">
-						<img src="img/workshops/lou.jpg" class="team-pic circle responsive-img">
+						<img src="img/workshops/3ed/manu.jpg" class="team-pic circle responsive-img">
 					</div>
-					<h3 class="team-name">Lou Bezerra</h3>
-					<p class="team-function">Hip Hop</p>
-					<div class="text">
+					<h3 class="team-name">Manu Gadelha</h3>
+					<p class="team-function">Hip Hop Divas</p>
+					<!-- <div class="text">
 						<p class="team-desc">Professor da Lia Ary Dance Studio<br>Dançarino do Side Project<br>Tetra Campeão do FCH2</p>
-					</div>
+					</div> -->
 				</div>
 				<!-- /.team-member -->
-			</div>
-			<div class="box col xl3 l3 m6 s12">
-				<div class="team-member hvr-grow" >
-					<div class="image">
-						<img src="img/workshops/rafa.jpg" class="team-pic circle responsive-img">
-					</div>
-					<h3 class="team-name">Rafael Nogueira</h3>
-					<p class="team-function">Contemporâneo</p>
-					<div class="text">
-						<p class="team-desc">Diretor e Coreógrafor do Grupo Libertos<br>Formado pela Battery Dance Company<br>Coreógrafo Revelação Fendafor 2015</p>
-					</div>
-				</div>
-				<!-- /.team-member -->
-			</div>
+			</div4
 		</div>	
 	</div>
 </section>
