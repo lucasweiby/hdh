130560fa602ed4123a39ae08e1bd2daf52b6fc86
lucasweiby <lucas_weiby@hotmail.com>
2018-02-15 16:42:55 -0300

    Form de inscricao

--- assets/nav.php
+++ assets/nav.php
@@ -10,7 +10,7 @@
 			<li><a class="nav-item" data-position="bottom" data-delay="50" href="#sobre">Sobre</a></li>
 			<!-- <li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Workshops" href="#convidados">Convidados</a></li>
 				<li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Programação" href="#programacao">Programação</a></li> -->
-				<li><a class="nav-item" data-position="bottom" data-delay="50" href="#modal1">Inscreva-se</a></li>
+				<li><a class="nav-item modal-trigger" data-position="bottom" data-delay="50" href="#modal-edit">Inscreva-se</a></li>
 				<li><a class="nav-item" data-position="bottom" data-delay="50" href="#contact">Contato</a></li>
 			</ul>
 
@@ -25,12 +25,267 @@
 	</nav>
 
 	<!-- Modal Structure -->
-	<div id="modal1" class="modal">
+	<div id="modal-edit" class="modal modal-fixed-footer">
 		<div class="modal-content">
 			<h4>Edital - Hana Dul Hallyu - 3ª Edição</h4>
-			<p>A bunch of text</p>
+			<p>
+				Este edital tem como finalidade definir as condições para participação, regras e informações gerais das performances com caráter competitivo que acontecerão na 
+				3ª edição do evento cultural Hana Dul Hallyu.
+
+				<br>
+
+				<h5>1. DO EVENTO</h5>
+
+				As apresentações acontecerão no palco principal durante a 3º edição do evento cultural Hana Dul Hallyu, no decorrer do dia 10 de Março de 2018 no Theatro José de Alencar, em Fortaleza-CE.
+
+
+				<h5>2. DAS COMPETIÇÕES</h5>
+
+				As competições serão divididas nas seguintes categorias: Pocket Dance, Dance Cover, Canto e Estilo Livre; com suas devidas limitações e critérios de julgamento e participação.
+
+				<br><br>
+
+				<b>2.1. Pocket Dance</b> <br><br>
+
+				Apresentações de danças com a participação de 1 (um) a 3 (três) dançarino(s) e uma performance com tempo máximo de 2 minutos e 15 segundos, caracterizando uma apresentação “compacta”. Nesta categoria os competidores deverão apresentar a coreografia originalmente executada pelo artista de K-pop. <br><br>
+
+
+				*Não será permitida a inscrição do participante em mais de uma apresentação nesta modalidade. <br><br>
+
+
+				Integrantes: Mínimo 1 (um), Máximo 3 (três); <br>
+				Áudio: Mínimo 1 (um) minuto, Máximo 2 (dois) minutos e 15 (quinze) segundos. <br><br>
+
+
+
+
+
+				<b>2.2. Dance Cover</b><br><br>
+
+				Apresentações de performances (dança e canto: dublagem) no quesito Cover de grupos de K-pop. Nesta categoria, os competidores deverão apresentar a coreografia originalmente executada pelos artistas. 	<br><br>
+
+				*É permitida a inscrição de uma mesma pessoa em mais de um grupo cover, sob o pagamento de taxa pré-estabelecida no tópico  “3.5.” deste edital. <br><br>
+
+				Integrantes: Mínimo 4 (quatro), Máximo 20;
+				Áudio: Mínimo 3 (três) minutos, Máximo 5 minutos. <br><br>
+
+
+
+				<b>2.3. Canto</b> <br><br>
+
+				Os participantes deverão cantar e interpretar uma música cujo artista original seja de origem Coreana. O áudio deverá conter somente a versão instrumental da música, em boa qualidade, sendo permitida versões acústicas e/ou remix. <br><br>
+
+
+				*Não será permitida a inscrição do participante em mais de uma apresentação nesta modalidade. <br><br>
+
+				Integrantes: Mínimo 1 (um), Máximo 5 (cinco);
+				Áudio: Mínimo 2 (dois) minutos e 30 (trinta) segundos, Máximo 5 (cinco) minutos. <br><br>
+
+
+
+				<b>2.4. Estilo Livre</b> <br><br>
+
+				Esta categoria é reservada para apresentações de danças com gêneros musicais e coreográficos livres. A competição será mista entre solos, duos, trios e conjuntos. <br><br>
+
+				*É permitida a inscrição de uma mesma pessoa em mais de uma apresentação, sob o pagamento de taxa pré-estabelecida no item “3.5.” deste edital.
+				*Nesta categoria, o estilo K-pop não é permitido. <br><br>
+
+				Integrantes: Mínimo 1 (um), Máximo 20 (vinte);
+				Áudio: Mínimo 2 (dois) minutos e 30 (trinta) segundos, Máximo 5 (cinco) minutos. <br><br>
+
+
+				<h5>3. DAS INSCRIÇÕES</h5> <br><br>
+
+
+				<b>3.1.</b> O período de inscrição para a 3ª edição do Hana Dul Hallyu será de 15 a 20 de Fevereiro de 2018, ou enquanto durarem as vagas. <br>
+
+				<b>3.1.1.</b> Não haverá prazo para alteração/correção destas inscrições. <br>
+
+				<b>3.1.2.</b> Serão invalidadas inscrições com erros de arquivos e/ou falta de informações previamente solicitadas no ponto “4.7.” deste edital. <br>
+
+				<b>3.2.</b> Os competidores da 2ª edição do Hana Dul Hallyu - categoria Dance Cover - terão vaga garantida na 3ª edição do evento, mediante inscrição, que realizar-se-á exclusivamente no dia 18 de Janeiro de 2017, até as 23:59 horas do dia 18 de Janeiro de 2017. <br>
+
+				<b>3.2.1.</b> Não haverá prazo para alteração/correção destas inscrições. <br>
+
+				<b>3.2.2.</b> O grupo que não cumprir este prazo perderá sua vaga exclusiva e passará a concorrer com as demais inscrições (4.1.). <br>
+
+				<b>3.2.3.</b> Serão invalidadas inscrições com erros de arquivos e/ou falta de informações previamente solicitadas no ponto “3.6.” deste edital. <br>
+
+				<b>3.3.</b> As inscrições serão realizadas através do site www.hanadulhallyu.com.br <br>
+
+				<b>3.4.</b> Com seu ingresso de competidor, o(a) dançarino(a) poderá participar das 3 (três) categorias de dança e ainda participar do canto sem pagar nenhum acréscimo; precisando apenas respeitar as exigências de cada categoria e o prazo de inscrição. <br>
+
+				<b>3.5.</b> Se o(a) dançarino(a) for competir em 2 (dois) grupos na mesma categoria, será necessário pagar uma taxa no valor simbólico de R$5,00 (cinco reais) por grupo que participar. [Exemplo: Beltrano competirá em dois grupos na categoria Dance Cover. Ele pagará o valor do seu ingresso normal + R$5,00. E se Beltrano também competir em dois grupos na categoria Estilo Livre, além do Dance Cover? Beltrano pagará o valor do seu ingresso normal + R$10,00 (dois grupos extras: um do Dance Cover e um do Estilo Livre). <br>
+
+				<b>3.5.1.</b> Essa taxa deverá ser paga no ato da compra do seu ingresso de competidor. (Avisaremos as datas das reuniões na nossa página no Facebook: /hanadulevent ) <br>
+
+				<b>3.6.</b> Para a inscrição será solicitado as seguintes informações: <br><br>
+
+				• Categoria da competição; <br>
+				• Nome do grupo ou nome artístico do competidor; <br>
+				• E-mail do representante; <br>
+				• Quantidade de integrantes; <br>
+				• Nome completo de todos os integrantes com número do documento oficial com foto, que será apresentado no dia do evento (aceito: RG, passaporte, reservista, CNH e carteirinha de estudante vigente); <br>
+				• Arquivo de áudio da música a ser apresentada, respeitando o limite de tempo da categoria (ver ponto “4.1.” deste edital); <br>
+				• Arquivo de imagem do figurino referência. Serão aceitas fotos e croquis (ver ponto “4.2.” deste edital); <br>
+				• Link do vídeo referência que mostre a coreografia a ser apresentada (ver ponto “4.3.” deste edital); <br>
+				• Arquivo de imagem (sem fundo, formato .png) contendo o LOGO do grupo (caso tenha); <br>
+				• Nome do coreógrafo (categoria Estilo Livre). <br><br>
+
+
+				<b>3.7.</b> Não haverá devolução do valor investido (seja de inscrições ou taxas) em caso de desistência por parte do grupo ou participante individual. <br>
+
+				<b>3.8.</b> A divulgação das inscrições validadas será realizada nas redes sociais do evento, a partir do dia 25 de Fevereiro de 2018. Sem horário definido. <br><br>
+
+
+				<h5>4. DOS ARQUIVOS</h5> <br><br>
+
+				<b>4.1. ÁUDIOS</b> <br><br>
+
+				<b>4.1.1.</b> As músicas que serão apresentadas na 3º edição do evento Hana Dul Hallyu, com exceção da Categoria Estilo Livre, deverão ser originalmente interpretadas por artistas coreanos; <br>
+
+				<b>4.1.2.</b> Só serão admitidas músicas cantadas nos idiomas coreano, japonês e inglês para as categorias Pocket, Dance Cover e Canto. Não existem limitações de idioma para a categoria Livre; <br>
+
+				<b>4.1.3.</b> Os arquivos de áudio deverão ser enviados à organização no formato .mp3 no ato da inscrição, respeitando os limites de tempo da categoria em que esteja realizando a inscrição; <br>
+
+				<b>4.1.4.</b> Cada inscrição deverá conter somente um arquivo de áudio; <br>
+
+				<b>4.1.5.</b> Não será permitida a mudança de música e/ou arquivo de áudio após envio da inscrição. <br><br>
+
+
+				<b>4.2. DOS FIGURINOS</b> <br><br>
+
+				<b>4.2.1.</b> Deverão ser enviados, no ato da inscrição, arquivos de imagem (no formato .png ou .jpeg) contendo o figurino oficial a ser apresentado nas competições, conforme o ponto “6.1.3” deste edital. <br> 
+
+				<b>4.2.2.</b> Na categoria Estilo Livre e Canto não será necessário o envio de figurino. <br><br>
+
+
+
+
+
+				<b>4.3. DOS LINKS</b> <br><br>
+
+				<b>4.3.1.</b> O vídeo referência da coreografia deverá ser do site: www.youtube.com ou de sites semelhantes (não download). <br>
+
+				<b>4.3.2.</b> Ao enviar links de sites de hospedagem para o download dos arquivos, certificar-se de que os links permanecerão válidos até dia 11 de Março de 2018. <br>
+
+				<b>4.3.3.</b> Se os links não estiverem acessíveis, a inscrição será invalidada. <br><br>
+
+
+				<h5>5. DAS VAGAS</h5> <br><br>
+
+				<b>5.1.</b> As vagas serão preenchidas pela ordem de envio do formulário de inscrição (respeitando os prazos estipulados nos pontos “4.1 e 4.2” deste edital). <br>
+
+				<b>5.2.</b> Se o número de inscritos ultrapassar a quantidade de vagas estabelecidas neste edital, as inscrições sobressalentes ficarão em Lista de Espera. <br><br>
+
+				<b>5.2.1.</b> No caso de desistência, a vaga será preenchida seguindo a ordem de inscrição na Lista de Espera. A comunicação de participação para inscrições em Lista de Espera será realizada através do e-mail do responsável informado no ato da inscrição. <br><br>
+
+				<b>5.3.</b> Serão disponibilizadas as seguintes vagas na 3ª edição do Hana Dul Hallyu:  <br><br>
+
+
+				• DANCE COVER - 20 Vagas. <br> 
+				• POCKET DANCE - 20 Vagas. <br>
+				• CANTO - 10 Vagas. <br>
+				• LIVRE - 10 Vagas. <br><br>
+
+				*As vagas da categoria Dance Cover serão preenchidas, primeiramente, pelos grupos que competiram na 2ª edição (conforme foi dito no ponto “1.3”). Avisaremos em nossas redes sociais os números de vagas que ainda serão preenchidas por novas inscrições. <br><br>
+
+
+				<h5>6. DO JULGAMENTO</h5> <br><br>
+
+				Em todas as categorias teremos um jurado especializado em cada critério de avaliação e caberá somente ao especialista pontuar no respectivo critério. <br><br>
+
+				<b>6.1. Nas categorias Pocket Dance e Dance Cover, serão avaliados: </b> <br><br>
+
+				<b>6.1.1. Desenvoltura: Jurado professor de dança no estilo. </b> <br><br>
+				Neste quesito, serão avaliados a desenvoltura dos dançarinos na execução dos passos, a sincronia e harmonia com a música e com os outros integrantes quando se tratar de duplas, trios e grupos. <br><br>
+
+				<b>6.1.2. Expressão artística (Fidelidade): Jurado cover de  Kpop. </b>  <br><br>
+				Neste quesito será avaliado: dublagem, expressão corporal de acordo com conceito da música escolhida, fidelidade ao(s) respectivo(s) artista(s) sul-coreano que originalmente interpreta a música escolhida. <br><br>
+
+				<b>6.1.3. Figurino: Jurado responsável com conhecimento em confecção de roupas e do gênero K-pop. </b> <br><br>
+				Neste quesito será avaliado a fidelidade do figurino em que o(s) participante(s) da categoria Pocket e Cover Dance se inspiraram para fazer o figurino com que estão se apresentando. <br><br>
+
+				<b>ATENÇÃO:</b> Para a categoria Pocket o figurino será julgado como base ou referência, se fazendo necessário o envio da foto no ato da inscrição. <br><br>
+
+
+
+				<b>6.2. Na categoria Canto serão avaliados: </b> <br><br>
+
+				<b>6.2.1. Afinação: Jurado cantor e instrumentista. </b> <br><br>
+				Neste critério será avaliado a afinação e ritmo do participante em relação à música escolhida. <br><br>
+
+				<b>6.2.2. Expressão artística: Jurado cantor e instrumentista. </b> <br><br>
+				Neste quesito será avaliado expressão de acordo com conceito da música escolhida, comportamento do(s) cantores no palco e interação com o público. <br><br>
+
+				<b>6.2.3. Pronúncia: Jurando familiarizado com os idiomas coreano, japonês e inglês. </b> <br><br>
+				Neste quesito será avaliado a pronúncia de acordo com o idioma da música escolhida. <br><br>
+
+
+				<b>6.3. Na categoria Estilo Livre serão avaliados: </b> <br><br>
+
+				<b>6.3.1. Composição Coreográfica: Jurado professor de dança e coreógrafo.</b> <br><br>
+				Neste quesito serão avaliados os movimentos, expressividade e técnica  dentro do estilo apresentado. No ato da inscrição informar nome do coreógrafo. <br><br>
+
+				<b>6.3.2. Sincronia: Jurado professor de dança.</b> <br><br>
+				Homogeneidade técnica entre os membros do grupo e a boa distribuição dos dançarinos no espaço durante as diferentes formações. <br><br>
+
+				<b>6.3.3. Apresentação/Figurino: estudante de moda com conhecimentos em dança.</b> <br><br>
+				O equipamento (roupa, acessórios e pintura) deve ter uma aparência e estética atrativa e original, adequada a música, ao corpo do(s) dançarino(s) e ao estilo de dança apresentado.  <br><br>
+
+
+				<b>6.4. Considerações Gerais </b> <br><br>
+
+				<b>6.4.1.</b> Todas as categorias contam com 3 (três) critérios de avaliação, e cada critério terá   pontuação de 0 à 10 pontos. Para a pontuação final será utilizado o cálculo de média aritmética, entendendo-se que todos os critérios terão a mesma influência na média final. <br><br>
+
+				<b>[Exemplo] Categoria Cover Dance - Grupo X: </b>
+				<b>Desenvoltura:</b> 9,0 pts | Expressão artística: 8,25 pts | Figurino: 10 pts <br>
+				<b>Média final:</b> 9,0 + 8,25 + 10 = 27,25 pts dividido por 3 (quantidade de critérios avaliativos), a pontuação total no Grupo X será  9,08 pts.<br><br>
+
+				<b>6.4.1.1.</b> Em caso de empate - entre os 05 primeiros colocados da categoria em questão -, os jurados adotarão a inclusão de 1,0 (um) ponto extra pela apresentação que mais se destacar na categoria. <br><br>
+
+
+
+				<h5>7. DAS PREMIAÇÕES</h5>
+
+				<b>7.1.</b> O pódio será conquistado pelos três primeiros colocados de cada categoria. <br>
+
+				<b>7.2.</b> Tabela oficial de premiações da 3ª edição do Hana Dul Hallyu: <br><br>
+
+
+				<b>DANCE COVER</b> <br><br>
+				• 1º Lugar - R$ 800,00 <br>
+				• 2º Lugar - R$ 300,00 <br>
+				• 3º Lugar - R$ 100,00 <br><br>
+				<b>POCKET DANCE & CANTO</b> <br><br>
+				• 1º Lugar - R$ 100,00 <br>
+				• 2º Lugar - BRINDE <br>
+				• 3º Lugar - BRINDE <br><br>
+				<b>LIVRE</b> <br><br>
+				• 1º Lugar - R$ 200,00 <br>
+				• 2º Lugar - BRINDE <br>
+				• 3º Lugar - BRINDE <br><br>
+
+				<b>7.3.</b> As premiações em dinheiro serão entregues aos campeões através de depósito bancário. (Após a conclusão do evento, a organização entrará em contato com os campeões). <br>
+
+				<b>7.4.</b> É de extrema importância que o campeão (seja grupo ou competidor individual) permaneça até o final do evento para receber seu prêmio. Caso contrário, o campeão perderá o direito ao prêmio. <br>
+
+				<b>7.4.1.</b> Um representante oficial do campeão (seja grupo ou competidor individual) será aceito para compor o pódio e receber o prêmio. <br><br>
+
+
+				<h5>8. DA COMUNICAÇÃO</h5> <br><br>
+
+				<b>8.1.</b> Para se comunicar com a organização do evento, utilizem as redes sociais: Facebook ( /hanadulevent ), Instagram ( @hanadulhallyu ), 
+				Site ( www.hanadulhallyu.com ) e Email ( hanadulhallyu@gmail.com ).  <br>
+
+				<b>8.2.</b> A organização do Hana Dul Hallyu fica isenta de informar as inscrições invalidadas por motivos de arquivos inválidos e/ou falta de informações. <br>
+
+				<b>8.3.</b> Os meios de divulgação oficial das informações sobre o evento são: Página no Facebook, Instagram e Site. Além disso, é informação não confiável. <br><br>
+
+
+			</p>
 		</div>
 		<div class="modal-footer">
-			<a href="" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+			<a href="inscricao.php" class="modal-action modal-close waves-effect waves-green btn-flat">Estou ciente e quero prosseguir</a>
 		</div>
 	</div>
