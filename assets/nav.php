b77743540ae107ff924557b899482c3ecf5ade9f
lucasweiby <lucas_weiby@hotmail.com>
2018-02-15 17:42:18 -0300

    Form de Inscricao

--- assets/nav.php
+++ assets/nav.php
@@ -10,14 +10,14 @@
 			<li><a class="nav-item" data-position="bottom" data-delay="50" href="#sobre">Sobre</a></li>
 			<!-- <li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Workshops" href="#convidados">Convidados</a></li>
 				<li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Programação" href="#programacao">Programação</a></li> -->
-				<!-- <li><a class="nav-item modal-trigger" data-position="bottom" data-delay="50" href="#modal-edit">Inscreva-se</a></li> -->
+				<li><a class="nav-item modal-trigger" data-position="bottom" data-delay="50" href="#modal-edit">Inscreva-se</a></li>
 				<li><a class="nav-item" data-position="bottom" data-delay="50" href="#contact">Contato</a></li>
 			</ul>
 
 			<ul id="mobile-demo" class="side-nav">
 				<li><a class="nav-item" href="index.php"><i class="material-icons">home</i>Início</a></li>
 				<li><a class="nav-item" href="#sobre"><i class="large material-icons">feedback</i>Sobre nós</a></li>
-				<!-- <li><a class="nav-item modal-trigger" data-position="bottom" data-delay="50" href="#modal-edit"><i class="material-icons">insert_invitation</i>Inscreva-se</a></li> -->
+				<li><a class="nav-item modal-trigger" data-position="bottom" data-delay="50" href="#modal-edit"><i class="material-icons">insert_invitation</i>Inscreva-se</a></li>
 			<!-- <li><a class="nav-item" href="#convidados"><i class="large material-icons">group_work</i>Workshops</a></li>
 				<li><a class="nav-item" href="#programacao"><i class="large material-icons">timer</i>Programação</a></li> -->
 				<li><a class="nav-item" href="#contact"><i class="large material-icons">perm_phone_msg</i>Contato</a></li>
