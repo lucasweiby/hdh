8a7fa54d14cab54c7d8f73ca5e288dc9f6225f4f
lucasweiby <lucas_weiby@hotmail.com>
2018-02-15 15:34:08 -0300

    Form de inscricao

--- assets/nav.php
+++ assets/nav.php
@@ -9,18 +9,28 @@
 			<li><a class="nav-item" data-position="bottom" data-delay="50" href="index.php">Início</a></li>
 			<li><a class="nav-item" data-position="bottom" data-delay="50" href="#sobre">Sobre</a></li>
 			<!-- <li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Workshops" href="#convidados">Convidados</a></li>
-			<li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Programação" href="#programacao">Programação</a></li> -->
-			<li><a class="nav-item" data-position="bottom" data-delay="50" href="#">Inscreva-se</a></li>
-			<li><a class="nav-item" data-position="bottom" data-delay="50" href="#contact">Contato</a></li>
-		</ul>
+				<li><a class="nav-item tooltipped" data-position="bottom" data-delay="50" data-tooltip="Programação" href="#programacao">Programação</a></li> -->
+				<li><a class="nav-item" data-position="bottom" data-delay="50" href="#modal1">Inscreva-se</a></li>
+				<li><a class="nav-item" data-position="bottom" data-delay="50" href="#contact">Contato</a></li>
+			</ul>
 
-		<ul id="mobile-demo" class="side-nav">
-			<li><a class="nav-item" href="index.php"><i class="material-icons">home</i>Início</a></li>
-			<li><a class="nav-item" href="#sobre"><i class="large material-icons">feedback</i>Sobre nós</a></li>
+			<ul id="mobile-demo" class="side-nav">
+				<li><a class="nav-item" href="index.php"><i class="material-icons">home</i>Início</a></li>
+				<li><a class="nav-item" href="#sobre"><i class="large material-icons">feedback</i>Sobre nós</a></li>
 			<!-- <li><a class="nav-item" href="#convidados"><i class="large material-icons">group_work</i>Workshops</a></li>
-			<li><a class="nav-item" href="#programacao"><i class="large material-icons">timer</i>Programação</a></li> -->
-			<li><a class="nav-item" href="#contact"><i class="large material-icons">perm_phone_msg</i>Contato</a></li>
-		</ul>
-	</div>
-</nav>
+				<li><a class="nav-item" href="#programacao"><i class="large material-icons">timer</i>Programação</a></li> -->
+				<li><a class="nav-item" href="#contact"><i class="large material-icons">perm_phone_msg</i>Contato</a></li>
+			</ul>
+		</div>
+	</nav>
 
+	<!-- Modal Structure -->
+	<div id="modal1" class="modal">
+		<div class="modal-content">
+			<h4>Edital - Hana Dul Hallyu - 3ª Edição</h4>
+			<p>A bunch of text</p>
+		</div>
+		<div class="modal-footer">
+			<a href="" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+		</div>
+	</div>
--- inscricao.php
+++ inscricao.php
@@ -4,23 +4,22 @@
 	<main>
 		<section id="contact">
 			<div class="container">
-				<div class="row hide-on-small-only">
+				<div class="row">
 					<div class="col lg12 m12">
 						<div class="text-contact" style="text-align: center;">
 							<h1 class="um">Increva-se para a nossa 3ª edição!*</h1>
-							<p class="dois">*O atual período de inscrição é apenas para GRUPOS COVER que participaram da segunda edição.</p>
 						</div>
 					</div>
 
 					<div class="col lg12 m12">
 						<form id="form-contact" class="margem col lg12 m12 s12 insc" name="form_contato" method="post">
-							<h3 class="integrantes">Dados do grupo</h3>
+							<h3 class="integrantes">Dados do Competidor</h3>
 							<div class="row">
-								<div class="input-field col lg6 m6 s6">
+								<div class="input-field col lg6 m6 s12">
 									<input id="nome" name="nome" type="text" class="validate" required="required">
 									<label for="name">Nome do grupo ou participante</label>
 								</div>
-								<div class="input-field col lg6 m6 s6">
+								<div class="input-field col lg6 m6 s12">
 									<select name="categoria" id="categoria" required="required">
 										<option value="" disabled selected>Escolha uma categoria</option>
 										<option value="Pocket dance">Pocket dance</option>
@@ -32,35 +31,35 @@
 								</div>
 							</div>
 							<div class="row">
-								<div class="input-field col lg5 m5 s5">
+								<div class="input-field col lg5 m5 s12">
 									<input id="nome_rep" name="nome_rep" type="text" class="validate" required="required">
 									<label for="nome_rep">Nome do representante + Nome do grupo (para confirmação)</label>
 								</div>
-								<div class="input-field col lg5 m5 s5">
+								<div class="input-field col lg5 m5 s8">
 									<input id="email_rep" name="email_rep" type="email" class="validate" required="required">
 									<label for="email" data-error="E-mail inválido" data-success="Este e-mail é válido">E-mail do representante</label>
 								</div>
-								<div class="input-field col lg2 m2 s2">
+								<div class="input-field col lg2 m2 s4">
 									<input id="n_integ" name="n_integ" type="number" class="validate" required="required">
-									<label for="integ" data-error="Digite apenas números">Quant. de integrantes</label>
+									<label for="integ" data-error="Digite apenas números">Nº de integrantes</label>
 								</div>
 							</div>
 
 							<h3 class="integrantes">Integrantes</h3>
 							<div class="integrantes_cadastro">
 								<div class="row">
-									<div class="input-field col s1">
+									<div class="input-field col m1 s1">
 										<label for="nome_integ1">1</label>
 									</div>
-									<div class="input-field col s7">
+									<div class="input-field col m7 s7">
 										<input id="nome_integ1" name="nome_integ1" type="text" class="validate" required="required">
 										<label for="nome_integ1">Nome do integrante</label>
 									</div>
-									<div class="input-field col s3">
+									<div class="input-field col m3 s4">
 										<input id="n_integ1" name="n_integ1" type="number" class="validate" required="required">
 										<label for="n_integ1" data-error="Digite apenas números">Nº documento oficial com foto</label>
 									</div>
-									<div class="input-field col s1">
+									<div class="input-field col m1 s12">
 										<a id="botao-mais" class="btn-floating waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Inserir novo integrante" style="background-color: #F71290;"><i class="material-icons">add</i></a>
 									</div>
 								</div>
