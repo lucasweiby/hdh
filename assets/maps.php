e251874e5bd236686b16f734ecf42eb601c6d2bc
Lucas Weiby <lucas_weiby@hotmail.com>
2018-07-23 12:42:52 -0300

    Update maps.php

--- assets/maps.php
+++ assets/maps.php
@@ -1,15 +1,15 @@
 <section id="map-sec" class="map">
 	<div class="row">
 		<div class="col xl12 l12 m12 s12">
-			<h3 class="map-title">E onde será a 3ª edição do Hana, Dul... Hallyu?</h3>
-			<p class="end"><i class="material-icons">location_on</i>Theatro José de Alencar - R. Liberato Barroso, 525 - Centro, Fortaleza - CE, 60030-160</p>
+			<h3 class="map-title">E onde será a 4ª edição do Hana, Dul... Hallyu?</h3>
+			<p class="end"><i class="material-icons">location_on</i>Teatro do Centro Cultural Dorian Sampaio - R. Édson Queirós - Centro, Maracanaú - CE, 61905-155</p>
 		</div>
 	</div>
 	<div class="row">
 		<div id="map" class="map-view">
 			<script>
 				function initMap() {
-					var uluru = {lat: -3.7277855, lng: -38.5316756};
+					var uluru = {lat: -3.8802798, lng: -38.6325226};
 					var map = new google.maps.Map(document.getElementById('map'), {
 						zoom: 15,
 						center: uluru
